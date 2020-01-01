5fd58a81f18409d07d0f7ac3063806401a11b234
Alexander Meindl <alex@alphanodes.com>
2020-01-01 18:50:23 +0100

    Test redmine master and add uninstall to travis

--- test/unit/i18n_test.rb
+++ test/unit/i18n_test.rb
@@ -17,11 +17,7 @@ class I18nTest < ActiveSupport::TestCase
   end
 
   def test_locales_validness
-    lang_files_count = Dir[Rails.root.join('plugins',
-                                           'redmine_privacy_terms',
-                                           'config',
-                                           'locales',
-                                           '*.yml')].size
+    lang_files_count = Dir[Rails.root.join('plugins/redmine_privacy_terms/config/locales/*.yml')].size
     assert_equal lang_files_count, 2
     valid_languages.each do |lang|
       assert set_language_if_valid(lang)
